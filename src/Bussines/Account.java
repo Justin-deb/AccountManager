6a2cb42da55d96e31a52279b47a45a17b8e64bbb
String-Bl4z3-deb <justinestradacruz2006@gmail.com>
2024-10-18 20:04:45 -0600

    <Feat> Added a variables to Account.java, constructors, getters and setters, and started funtions saveFile and readFile

--- src/Bussines/Account.java
+++ src/Bussines/Account.java
@@ -1,13 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Bussines;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author justin
  */
 public class Account {
+    private String name;
+    private int ID;
+    private String password;
+    private double balance;
+
+    public Account() {
+    }
+
+    public Account(String name, int ID, String password, double balance) {
+        this.name = name;
+        this.ID = ID;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "name=" + name + ", ID=" + ID + ", password=" + password + ", balance=" + balance + '}';
+    }
+    
+    public void saveFile(String information){
+
+    }
+    
+    public ArrayList<Account> readFile(){
+        return null;
+    }
     
 }
